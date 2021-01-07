81d8a0d723a4447bed74ce92d723221be6326ed8
nalmt <nabil.lamrabet@pm.me>
2021-01-07 19:00:29 +0100

    fix main

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ from blackbox import check
 
 STUDENT_ID = 11806768
 
-def check_list(list_password):
+def check_list(student, list_password):
     results = []
     for password in list_password:
-        results.append(check(STUDENT_ID, password))
+        results.append(check(student, password))
     return results
 
-# Génération aléatoire d'une population
+a = check_list(STUDENT_ID, ["ALGOGEN", "PASSWORD"])
+
+print(a)
+
+Génération aléatoire d'une population
 
 population = generate_population()
 
 while 1 not in check_list(STUDENT_ID, population):
-    nouvelle population = []
+    nouvelle_population = []
     for individu in population:
         # TODO
     population = nouvelle_population
