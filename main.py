479fafc3c8d8ac093fd1ad6210692e277bfe72c5
nalmt <nabil.lamrabet@pm.me>
2021-01-14 15:17:26 +0100

    code structure

--- main.py
+++ main.py
@@ -22,8 +22,20 @@ population = generate_population()
 
 while 1 not in check_list(STUDENT_ID, population):
     nouvelle_population = []
-    for individu in population:
-        # TODO
-    population = nouvelle_population
+
+    # tant que taille nouvelle population < population
+    # TODO : exploitation
+    # choisir premier parent au "roulette pondéré"
+    # choisir deuxième parent "roulette pondéré"
+
+    # Si random() < P(cross_over) (cross_over = 2)
+        #cross-over des deux parents et l'ajouter
+
+    # TODO : exploration
+    #  faire muter premier parent et l'ajouter
+    #  faire muter deuxième parent et l'ajouter
+    
+    # si taille population == taille nouvelle_population
+    # population = nouvelle population
 
 print(population)
