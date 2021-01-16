c9cf912a6f523fdfa584e146536f61f89a1c28f5
emnabarred <barredemna@gmail.com>
2021-01-16 10:57:25 +0100

    edit mutation

--- main.py
+++ main.py
@@ -70,35 +70,79 @@ def exploitation(parent1, parent2):
 
 def deleteGene(geneIndex, genotype):
     if len(genotype) >= 13:
-        genotype = genotype[:geneIndex] + genotype[geneIndex + 1:]
-
-
+        return genotype[:geneIndex] + genotype[geneIndex + 1:]
 
 def addGene(genotype):
     if len(genotype) <= 17:
-        genotype.join(random.choice(alphabets))
-
-
+        rand = random.choice([1, 2])
+        if rand == 1:
+            randomAlphabet = random.choice(alphabets)
+            genotype.join(randomAlphabet)
+        else:
+            randomNumber = str(random.choice(numbers))
+            genotype.join(randomNumber)
+        return genotype
 
 def mutate(gene):
+
     if gene in alphabets:
         indexAlpha = alphabets.index(gene)
+
+        #si l'aphabet n'est ni Z ni A
         if indexAlpha not in [0, 25]:
             rand = random.choice([1, 2])
             if rand == 1:
+                #muter le gene vers l'alphabet suivant
                 return alphabets[indexAlpha + 1]
-            else:
+            elif rand == 2:
+                # muter le gene vers l'alphabet précédent
                 return alphabets[indexAlpha - 1]
-        else:
-            return gene
-    elif gene in numbers:
-        indexNum = numbers.index(gene)
-        if indexNum not in [0, 9]:
+
+        #si l'aphabet est un A
+        elif indexAlpha == 0:
             rand = random.choice([1, 2])
             if rand == 1:
-                return numbers[indexNum + 1]
+                # muter le gene vers l'alphabet suivant (B)
+                return alphabets[1]
+            elif rand == 2:
+                # muter le gene vers l'alphabet précédent (Z)
+                return alphabets[25]
+
+        # si l'aphabet est un Z
+        elif indexAlpha == 25:
+            rand = random.choice([1, 2])
+            if rand == 1:
+                # muter le gene vers l'alphabet suivant (A)
+                return alphabets[0]
+            elif rand == 2:
+                # muter le gene vers l'alphabet précédent (Y)
+                return alphabets[24]
+
+        else:
+            return gene
+
+    elif int(gene) in numbers:
+        gene = int(gene)
+        if gene not in [0, 9]:
+            rand = random.choice([1, 2])
+            if rand == 1:
+                return gene + 1
             else:
-                return numbers[indexNum - 1]
+                return gene - 1
+
+        elif gene == 0:
+            rand = random.choice([1, 2])
+            if rand == 1:
+                return 1
+            else:
+                return 9
+
+        elif gene == 9:
+            rand = random.choice([1, 2])
+            if rand == 1:
+                return 0
+            else:
+                return 8
 
         else:
             return gene
@@ -106,21 +150,23 @@ def mutate(gene):
 
 def mutateGenotype(genotype):
     for geneIndex in range(0, len(genotype) - 1):
-        gene = genotype[geneIndex]
+        if geneIndex in range(0, len(genotype) - 1):
+            gene = genotype[geneIndex]
+
+            if random.random() < mutateProbabity:
+                newGene = str(mutate(gene))
+                genotype = genotype[:geneIndex] + str(newGene) + genotype[geneIndex + 1:]
 
 
-        if random.random() < mutateProbabity:
-            newGene = mutate(gene)
-            genotype = genotype[:geneIndex] + str(newGene) + genotype[geneIndex + 1:]
+            if random.random() < addDeleteProbabilty:
+                deleteIndex = random.randrange(0, len(genotype) - 1, 1)
+                rand = random.choice([1, 2])
+                if rand == 1:
+                    genotype = addGene(genotype)
+                else:
+                    genotype = deleteGene(deleteIndex, genotype)
 
-
-        if random.random() < addDeleteProbabilty:
-            deleteIndex = random.randrange(0, len(genotype) - 1, 1)
-            rand = random.choice([1, 2])
-            if rand == 1:
-                addGene(genotype)
-            else:
-                deleteGene(deleteIndex, genotype)
+    return genotype
 
 
 
