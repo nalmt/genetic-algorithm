81f72bcb468f2a0a459166c8d3ecc43e8dd3b804
nalmt <nabil.lamrabet@pm.me>
2021-01-31 15:50:52 +0100

    add generatePopulation, wheel + refactoring

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # coding : utf8
 # !/usr/bin/env python
-#import demo
 import subprocess
 import random
 import string
-
-alphabets = list(string.ascii_uppercase)
-numbers = list(range(10))
-mutateProbabity = 0.2
-addDeleteProbabilty = 0.2
+from crossover import *
+from mutation import *
 
 STUDENT_ID = 11806768
+POPULATION_SIZE = 15
+CROSSOVER_PROBABILITY = 0.2
 
 def check_list(student, list_password):
     proc = subprocess.Popen(["./unlock_mac", str(student)] + list_password, stdout=subprocess.PIPE)
@@ -22,170 +20,43 @@ def check_list(student, list_password):
         results.append(float(str(line).split("\\t")[1].split("\\n")[0]))
     return results
 
+# https://pynative.com/python-generate-random-string/
+def random_person(length):
+    letters_and_digits = string.ascii_uppercase + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
 
-a = check_list(STUDENT_ID, ["ALGOGEN", "PASSWORD"])
-
-print(a)
-
-# roulette pondérée
-roulette = []
-#Porbabilité de Crossover
-crossoverPropbabilty = 0,2
-
-def generatePopulation():
-    #TODO
+def generatePopulation(population_size):
+    population = []
+    for i in range(0, population_size):
+        random_length = random.randint(12, 18)
+        population.append(random_person(random_length))
     return population
 
-# tant que taille nouvelle population < population
-# TODO : exploitation
-# choisir premier parent au "roulette pondéré"
-# choisir deuxième parent "roulette pondéré"
-def exploitation():
-
-    parent1 = roulette[0]
-    parent2 = roulette[1]
-    return parent1, parent2
-
-    # Si random() < P(cross_over) (cross_over = 2)
-    # cross-over des deux parents et l'ajouter
-    if random.random() < crossoverPropbabilty:
-        cross = crossover(parent1, parent2)
-        population.add(cross)
-
-# TODO : exploration
-#  faire muterpremier parent et l'ajouter
-#  faire muter deuxième parent et l'ajouter
-def exploitation(parent1, parent2):
-    mutate(parent1)
-    population.addParent(parent1)
-    mutate(parent2)
-    population.addParent(parent2)
-
-
-    # si taille population == taille nouvelle_population
-    # population = nouvelle population
-    if len(population) == len(newPopulation):
-        population = newPopulation
-
-
-def deleteGene(geneIndex, genotype):
-    if len(genotype) >= 13:
-        return genotype[:geneIndex] + genotype[geneIndex + 1:]
-
-def addGene(genotype):
-    if len(genotype) <= 17:
-        rand = random.choice([1, 2])
-        if rand == 1:
-            randomAlphabet = random.choice(alphabets)
-            genotype.join(randomAlphabet)
-        else:
-            randomNumber = str(random.choice(numbers))
-            genotype.join(randomNumber)
-        return genotype
-
-def mutate(gene):
-
-    if gene in alphabets:
-        indexAlpha = alphabets.index(gene)
-
-        #si l'aphabet n'est ni Z ni A
-        if indexAlpha not in [0, 25]:
-            rand = random.choice([1, 2])
-            if rand == 1:
-                #muter le gene vers l'alphabet suivant
-                return alphabets[indexAlpha + 1]
-            elif rand == 2:
-                # muter le gene vers l'alphabet précédent
-                return alphabets[indexAlpha - 1]
-
-        #si l'aphabet est un A
-        elif indexAlpha == 0:
-            rand = random.choice([1, 2])
-            if rand == 1:
-                # muter le gene vers l'alphabet suivant (B)
-                return alphabets[1]
-            elif rand == 2:
-                # muter le gene vers l'alphabet précédent (Z)
-                return alphabets[25]
-
-        # si l'aphabet est un Z
-        elif indexAlpha == 25:
-            rand = random.choice([1, 2])
-            if rand == 1:
-                # muter le gene vers l'alphabet suivant (A)
-                return alphabets[0]
-            elif rand == 2:
-                # muter le gene vers l'alphabet précédent (Y)
-                return alphabets[24]
-
-        else:
-            return gene
-
-    elif int(gene) in numbers:
-        gene = int(gene)
-        if gene not in [0, 9]:
-            rand = random.choice([1, 2])
-            if rand == 1:
-                return gene + 1
-            else:
-                return gene - 1
-
-        elif gene == 0:
-            rand = random.choice([1, 2])
-            if rand == 1:
-                return 1
-            else:
-                return 9
-
-        elif gene == 9:
-            rand = random.choice([1, 2])
-            if rand == 1:
-                return 0
-            else:
-                return 8
-
-        else:
-            return gene
-
-
-def mutateGenotype(genotype):
-    for geneIndex in range(0, len(genotype) - 1):
-        if geneIndex in range(0, len(genotype) - 1):
-            gene = genotype[geneIndex]
-
-            if random.random() < mutateProbabity:
-                newGene = str(mutate(gene))
-                genotype = genotype[:geneIndex] + str(newGene) + genotype[geneIndex + 1:]
-
-
-            if random.random() < addDeleteProbabilty:
-                deleteIndex = random.randrange(0, len(genotype) - 1, 1)
-                rand = random.choice([1, 2])
-                if rand == 1:
-                    genotype = addGene(genotype)
-                else:
-                    genotype = deleteGene(deleteIndex, genotype)
-
-    return genotype
-
-
-
-
-
-
-
-#Ensuite on a une probabilité de supprimer un gène au hasard et une probabilité d'ajouter un gène au hasard (ces types de mutations s'appliquent uniquement si nous avons 13 à 17 gènes inclus).
-
-
-def crossover(parent1, parent2):
-    #TODO
+def roulette_wheel(population, number_to_select):
+    weights = check_list(STUDENT_ID, population)
+    return random.choices(population, weights, k=number_to_select)
 
 # Génération aléatoire d'une population
-population = generatePopulation()
-while 1 not in check_list(STUDENT_ID, population):
-    newPopulation = []
-    if len(newPopulation) < len(population):
-        exploitation()
+population = generatePopulation(POPULATION_SIZE)
 
+while 1 not in check_list(STUDENT_ID, population):
+    print(max(check_list(STUDENT_ID, population)))
+
+    newPopulation = []
+
+    while len(newPopulation) < POPULATION_SIZE:
+        person_1, person_2 = roulette_wheel(population, 2)
+        newPopulation.extend([person_1, person_2])
+
+        if random.random() < CROSSOVER_PROBABILITY:
+            person_3, person_4 = random_crossover(person_1, person_2, 8)
+            newPopulation.extend([person_3, person_4])
+
+        person_5 = mutateGenotype(person_1)
+        person_6 = mutateGenotype(person_2)
+        newPopulation.extend([person_5, person_6])
+
+    population = newPopulation
 
 print(population)
